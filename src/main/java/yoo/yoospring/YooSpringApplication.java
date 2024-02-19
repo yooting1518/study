e5e79e9b3c136fa6a084b9681c2b6ce2816674aa
yooting <yooting@naver.com>
2024-02-19 09:59:40 +0900

    first commit

---
+++ src/main/java/yoo/yoospring/YooSpringApplication.java
@@ -0,0 +1,13 @@
+package yoo.yoospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YooSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YooSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yoo/yoospring/YooSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package yoo.yoospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YooSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
